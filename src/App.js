5da6a8c2c535629e0faca132c3a8aaf22a34b38a
Seth <colts18seth@gmail.com>
2020-09-21 19:52:30 -0400

    added class App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-import JokeList from "./JokeList";
+// import React from "react";
+// import JokeList from "./JokeList";
 
-function App() {
-  return (
-    <div className="App">
-      <JokeList />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <JokeList />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import JokeList from './JokeList';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <JokeList />
+            </div>
+        )
+    }
 }
 
 export default App;
